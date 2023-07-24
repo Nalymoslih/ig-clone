d373c9704dae7dc0dd74041aa2c1cd926db7ca5b
Naly moslih <naly.moslih48@gmail.com>
2023-07-24 14:07:42 +0300

    push

--- instagramClone/App.js
+++ instagramClone/App.js
@@ -1,24 +1,8 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-import NewPostScreen from './screens/NewPostScreen';
-import HomeScreen from './screens/HomeScreen';
 import SignedInStack from './navigation';
-// import NewPostScreen from './screens/NewPostScreen';
 
 function App() {
-  // return <HomeScreen />;
-  // return <NewPostScreen />;
   return <SignedInStack />;
 }
-
-const styles = StyleSheet.create({});
 
 export default App;
