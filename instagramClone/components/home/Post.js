f94d9cb99460c50e1f9906a6cd43a3db21937143
Naly moslih <naly.moslih48@gmail.com>
2023-07-28 20:57:42 +0300

    push

--- instagramClone/components/home/Post.js
+++ instagramClone/components/home/Post.js
@@ -1,8 +1,6 @@
 import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
 import React from 'react';
-// import {Divider} from '@rneui/themed';
 import {Divider} from 'react-native-elements';
-// import POSTS from '../../data/posts';
 
 const postFooterIcons = [
   {
