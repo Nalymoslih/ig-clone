191e751eb965a6fa96b285f0ea003233ae45ab93
Naly moslih <naly.moslih48@gmail.com>
2023-07-28 19:24:30 +0300

    push

--- instagramClone/components/home/Stories.js
+++ instagramClone/components/home/Stories.js
@@ -1,27 +1,39 @@
-import {View, Text, ScrollView, Image, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  ScrollView,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
 import React from 'react';
 import {USERS} from '../../data/users';
 
+import {useNavigation} from '@react-navigation/native';
+
 const Stories = () => {
+  const navigation = useNavigation();
   return (
     <View style={{marginBottom: 20}}>
-      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-        {USERS.map((story, index) => (
-          <View key={index} style={{alignItems: 'center'}}>
-            <Image
-              style={styles.story}
-              source={{
-                uri: story.image,
-              }}
-            />
-            <Text style={{color: 'white', marginLeft: 10}}>
-              {story.user.length > 11
-                ? story.user.slice(0, 6).toLowerCase() + '...'
-                : story.user.toLowerCase()}
-            </Text>
-          </View>
-        ))}
-      </ScrollView>
+      <TouchableOpacity onPress={() => navigation.navigate('StoriePost')}>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          {USERS.map((story, index) => (
+            <View key={index} style={{alignItems: 'center'}}>
+              <Image
+                style={styles.story}
+                source={{
+                  uri: story.image,
+                }}
+              />
+              <Text style={{color: 'white', marginLeft: 10}}>
+                {story.user.length > 11
+                  ? story.user.slice(0, 6).toLowerCase() + '...'
+                  : story.user.toLowerCase()}
+              </Text>
+            </View>
+          ))}
+        </ScrollView>
+      </TouchableOpacity>
     </View>
   );
 };
