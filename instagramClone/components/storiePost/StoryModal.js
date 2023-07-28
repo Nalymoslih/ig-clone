4884e178a838301160f55d1c527a9d8592467974
Naly moslih <naly.moslih48@gmail.com>
2023-07-28 20:58:03 +0300

    push

---
+++ instagramClone/components/storiePost/StoryModal.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  Image,
+  Modal,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
+
+const StoryModal = ({visible, image, onClose}) => {
+  return (
+    <Modal visible={visible} animationType="fade" onRequestClose={onClose}>
+      <View style={styles.modalContainer}>
+        <Image style={styles.modalImage} source={{uri: image}} />
+        <TouchableOpacity style={styles.closeButton} onPress={onClose}>
+          <Text style={styles.closeButtonText}>Close</Text>
+        </TouchableOpacity>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.9)',
+  },
+  modalImage: {
+    width: 200,
+    height: 200,
+    borderRadius: 10,
+  },
+  closeButton: {
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+  },
+  closeButtonText: {
+    color: 'black',
+    fontWeight: 'bold',
+  },
+});
+
+export default StoryModal;
