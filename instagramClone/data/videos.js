b76b22f0d500b9671cc55f0388e799c351e10681
Naly moslih <naly.moslih48@gmail.com>
2023-07-30 18:14:06 +0300

    push

---
+++ instagramClone/data/videos.js
@@ -0,0 +1,30 @@
+export const videos = [
+  {
+    vdo: require('../storage/videos/video1.mp4'),
+    title: 'Naly_Moslih',
+    description: 'Feeling good today',
+    likes: '100k',
+    isLiked: false,
+  },
+  {
+    vdo: require('../storage/videos/video2.mp4'),
+    title: 'Yusf_jasim',
+    description: 'Feeling good today',
+    likes: '110k',
+    isLiked: false,
+  },
+  {
+    vdo: require('../storage/videos/video3.mp4'),
+    title: 'Naly_Moslih',
+    description: 'Feeling good today',
+    likes: '500k',
+    isLiked: false,
+  },
+  {
+    vdo: require('../storage/videos/video4.mp4'),
+    title: 'Yusf_jasim',
+    description: 'Feeling good today',
+    likes: '130k',
+    isLiked: false,
+  },
+];
