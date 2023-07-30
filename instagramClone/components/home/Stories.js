bc847f4a78dd2a42a169df63715bd08db13c88e6
Naly moslih <naly.moslih48@gmail.com>
2023-07-30 12:15:11 +0300

    push

--- instagramClone/components/home/Stories.js
+++ instagramClone/components/home/Stories.js
@@ -4,33 +4,113 @@ import {
   ScrollView,
   Image,
   StyleSheet,
+  Button,
   TouchableOpacity,
 } from 'react-native';
 import React, {useState} from 'react';
 import {USERS} from '../../data/users';
+import InstaStory from 'react-native-insta-story';
+import {shareStory} from 'react-native';
 
 import {useNavigation} from '@react-navigation/native';
 
 const Stories = () => {
   const navigation = useNavigation();
 
+  const data = [
+    {
+      user_id: 1,
+      user_image:
+        'https://pbs.twimg.com/profile_images/1222140802475773952/61OmyINj.jpg',
+      user_name: 'Ahmet Çağlar Durmuş',
+      stories: [
+        {
+          story_id: 1,
+          story_image:
+            'https://image.freepik.com/free-vector/universe-mobile-wallpaper-with-planets_79603-600.jpg',
+          swipeText: 'Custom swipe text for this story',
+          onPress: () => console.log('story 1 swiped'),
+        },
+        {
+          story_id: 2,
+          story_image:
+            'https://image.freepik.com/free-vector/mobile-wallpaper-with-fluid-shapes_79603-601.jpg',
+        },
+      ],
+    },
+    {
+      user_id: 2,
+      user_image:
+        'https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
+      user_name: 'Test User',
+      stories: [
+        {
+          story_id: 1,
+          story_image:
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjORKvjcbMRGYPR3QIs3MofoWkD4wHzRd_eg&usqp=CAU',
+          swipeText: 'Custom swipe text for this story',
+          onPress: () => console.log('story 1 swiped'),
+        },
+        {
+          story_id: 2,
+          story_image:
+            'https://files.oyebesmartest.com/uploads/preview/vivo-u20-mobile-wallpaper-full-hd-(1)qm6qyz9v60.jpg',
+          swipeText: 'Custom swipe text for this story',
+          onPress: () => console.log('story 2 swiped'),
+        },
+      ],
+    },
+  ];
+
+  const [seenStories, setSeenStories] = useState(new Set());
+
+  const updateSeenStories = ({story: {story_id}}) => {
+    setSeenStories(prevSet => {
+      prevSet.add(story_id);
+      return prevSet;
+    });
+  };
+
+  const handleSeenStories = async item => {
+    console.log(item);
+    const storyIds = [];
+    seenStories.forEach(storyId => {
+      if (storyId) storyIds.push(storyId);
+    });
+    if (storyIds.length > 0) {
+      await fetch('myApi', {
+        method: 'POST',
+        body: JSON.stringify({storyIds}),
+      });
+      seenStories.clear();
+    }
+  };
+
   return (
-    <View style={{marginBottom: 20}}>
-      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-        {USERS.map((story, index) => (
-          <TouchableOpacity
-            key={index}
-            onPress={() => navigation.push('StoriePost')}>
-            <Image style={styles.story} source={{uri: story.image}} />
-            <Text style={{color: 'white', marginLeft: 10}}>
-              {story.user.length > 11
-                ? story.user.slice(0, 6).toLowerCase() + '...'
-                : story.user.toLowerCase()}
-            </Text>
+    <InstaStory
+      data={data}
+      duration={10}
+      onStart={item => console.log(item)}
+      onClose={handleSeenStories}
+      onStorySeen={updateSeenStories}
+      renderCloseComponent={({item, onPress}) => (
+        <View style={{flexDirection: 'row'}}>
+          <TouchableOpacity>
+            <Text onPress={shareStory}>Share</Text>
+            <Text onPress={onPress}>X</Text>
           </TouchableOpacity>
-        ))}
-      </ScrollView>
-    </View>
+        </View>
+      )}
+      renderTextComponent={({item, profileName}) => (
+        <View>
+          <Text>{profileName}</Text>
+          <Text>{item.customProps?.yourCustomProp}</Text>
+        </View>
+      )}
+      style={{marginTop: 30}}
+    />
+
+    // TESt
   );
 };
 
