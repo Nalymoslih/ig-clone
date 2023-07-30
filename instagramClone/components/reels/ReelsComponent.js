44f3f3b22485efddc9b5bdf2bf76dd1a0ca8765d
Naly moslih <naly.moslih48@gmail.com>
2023-07-30 18:14:00 +0300

    push

---
+++ instagramClone/components/reels/ReelsComponent.js
@@ -0,0 +1,25 @@
+import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {SwiperFlatList} from 'react-native-swiper-flatlist';
+import {videos} from '../../data/videos';
+import SingleReel from './SingleReel';
+
+const ReelsComponent = () => {
+  const [cuurentIndex, setCurrentIndex] = useState(0);
+  const handleChangeIndexValue = index => {
+    setCurrentIndex(index);
+  };
+  return (
+    <SwiperFlatList
+      data={videos}
+      vertical={true}
+      onChangeIndex={handleChangeIndexValue}
+      renderItem={(item, index) => (
+        <SingleReel item={item} index={index} cuurentIndex={cuurentIndex} />
+      )}
+      keyExtractor={({item, index}) => index}
+    />
+  );
+};
+
+export default ReelsComponent;
