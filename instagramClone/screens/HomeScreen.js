d8d2818dc290f6a205517228defd2123007e74cc
Naly moslih <naly.moslih48@gmail.com>
2023-08-03 14:42:11 +0300

    push

--- instagramClone/screens/HomeScreen.js
+++ instagramClone/screens/HomeScreen.js
@@ -6,9 +6,6 @@ import Post from '../components/home/Post';
 import {POSTS} from '../data/posts';
 import BottomTabs, {bottomTabIcons} from '../components/home/BottomTabs';
 import {useNavigation} from '@react-navigation/native';
-// import Reels from './Reels';
-// import Reels from 'react-native-instagram-reels';
-// import {Videos} from '../data/videos';
 import Reels from './Reels';
 
 const HomeScreen = () => {
