5451a002eac55fe7304e6b7df1fdd9b9763e365d
Naly moslih <naly.moslih48@gmail.com>
2023-08-03 14:38:46 +0300

    push

--- instagramClone/components/reels/ReelsComponent.js
+++ instagramClone/components/reels/ReelsComponent.js
@@ -24,30 +24,3 @@ const ReelsComponent = () => {
   );
 };
 export default ReelsComponent;
-
-// const ReelsComponent = () => {
-//   const [cuurentIndex, setCurrentIndex] = useState(0);
-//   const handleChangeIndexValue = index => {
-//     setCurrentIndex(index);
-//   };
-//   return (
-//     <SwiperFlatList
-//       data={videos}
-//       vertical={true}
-//       viewabilityConfig={{
-//         // viewAreaCoveragePercentThreshold: 10,
-//         itemVisiblePercentThreshold: 1,
-//       }}
-//       onChangeIndex={handleChangeIndexValue}
-//       renderItem={({item, index}) => (
-//         <SingleReel
-//           key={index}
-//           item={item}
-//           index={index}
-//           cuurentIndex={cuurentIndex}
-//         />
-//       )}
-//       keyExtractor={({item, index}) => index}
-//     />
-//   );
-// };
