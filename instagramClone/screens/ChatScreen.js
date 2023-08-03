73f3170d8bf5669b84617e462e645ad2c284238b
Naly moslih <naly.moslih48@gmail.com>
2023-08-03 14:34:03 +0300

    push

--- instagramClone/screens/ChatScreen.js
+++ instagramClone/screens/ChatScreen.js
@@ -1,7 +1,6 @@
 import {View, Text} from 'react-native';
 import React from 'react';
 import Chat from '../components/chat/Chat';
-
 const ChatScreen = ({navigation}) => {
   return <Chat navigation={navigation} />;
 };
