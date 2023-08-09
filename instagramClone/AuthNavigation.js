5886312d0816c46e03f40a2dc0d8a9eb54436818
Naly moslih <naly.moslih48@gmail.com>
2023-08-09 17:36:36 +0300

    push

--- instagramClone/AuthNavigation.js
+++ instagramClone/AuthNavigation.js
@@ -20,32 +20,3 @@ const AuthNavigation = () => {
 };
 
 export default AuthNavigation;
-
-// import React, {useEffect, useState} from 'react';
-// import {SignedInStack, SignedOutStack} from './navigation';
-// import {View, Text} from 'react-native'; // Import any necessary components
-// const AuthNavigation = () => {
-//   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-//   useEffect(() => {
-//     // Call your backend API to check the user's authentication status
-//     fetch('http://localhost:3010/api/check-auth', {
-//       method: 'GET',
-//       headers: {
-//         Authorization: 'Bearer ' + 'yourActualAccessToken', // Replace with the actual token
-//       },
-//     })
-//       .then(response => response.json())
-//       .then(data => {
-//         // Set the isLoggedIn state based on the response
-//         setIsLoggedIn(data.isLoggedIn);
-//       })
-//       .catch(error => {
-//         console.error('Error checking authentication:', error);
-//       });
-//   }, []);
-
-//   return <View>{isLoggedIn ? <SignedInStack /> : <SignedOutStack />}</View>;
-// };
-
-// export default AuthNavigation;
